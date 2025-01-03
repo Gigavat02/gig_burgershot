1b627521ce18d915fcc4714167fc64ceef14f9c4
Gigavat <76479107+Gigavat02@users.noreply.github.com>
2025-01-03 16:46:31 +0200

    Job SQL

---
+++ install/job.sql
@@ -0,0 +1,6 @@
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+  (1000, 'burgershot', 0, 'burgershot', 'Employee', 20, '', '');
+
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+      ('burgershot', 'Burgershot', 1);
